74b0c84d84aa92fb26be22883b25b87adead0853
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-04-18 11:56:15 -0700

    create_user.sh  "SET GLOBAL local_infile"

--- create_user.sh
+++ create_user.sh
@@ -8,6 +8,7 @@ echo "Requires SUPERUSER privileges, i.e. a root password."
 echo "      (if no password, hit Enter at password prompt.)"
 echo ""
 
+mysql -u root -e 'SET GLOBAL local_infile=1'
 mysql -u root -p < create_user.sql
 
 echo ""
