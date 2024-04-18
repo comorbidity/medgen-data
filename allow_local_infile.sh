aedfb595deda1db3e9864b2b648aef09da0e8ad6
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-04-18 11:54:17 -0700

    added README comment about use of mysql INFILE

--- allow_local_infile.sh
+++ allow_local_infile.sh
@@ -1,1 +1,5 @@
 mysql -u root -e 'SET GLOBAL local_infile=1'
+
+# see also
+# https://www.basedash.com/blog/how-to-resolve-the-mysql-server-is-running-with-the-secure-file-priv-option-so-it-cannot-execute-this-statement 
+# secure-file-priv
