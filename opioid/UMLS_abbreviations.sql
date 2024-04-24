e0f6cb349a79dabdd07541eb81fecbef490fa246
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-04-24 16:05:54 -0700

    UMLS Semantic Network metadata infile loading is now cleaner.

--- opioid/UMLS_abbreviations.sql
+++ opioid/UMLS_abbreviations.sql
@@ -7,12 +7,12 @@ call log('umls_tui', 'https://lhncbc.nlm.nih.gov/ii/tools/MetaMap/Docs/SemanticT
 drop table if exists umls_tui;
 create table umls_tui
 (
- STY  varchar(4)	NOT NULL, 
- TUI  varchar(4)	NOT NULL,
- TUI_STR   varchar(50) 	NULL
+ STY        varchar(4)	NOT NULL,
+ TUI        varchar(4)	NOT NULL,
+ TUI_STR    varchar(50) NOT NULL
 );    
 
-load data local infile 'UMLS_TUI.tsv' into table umls_tui; 
+load data local infile 'UMLS_TUI.tsv' into table umls_tui ignore 1 lines;
 show warnings; 
 
 call create_index('umls_tui','TUI');
@@ -23,17 +23,12 @@ call log('umls_tty', 'Term Type in source');
 drop table if exists umls_tty;
 create table umls_tty
 (
- TTY  varchar(11)	NOT NULL,
- TTY_STR   varchar(50) 	NULL
+ TTY        varchar(11) NOT NULL,
+ TTY_STR    varchar(80) NOT NULL
 );    
 
-load data local infile 'UMLS_TTY.tsv'  into table umls_tty
-fields terminated by '\t'
-optionally enclosed by '"' 
-ESCAPED BY '' 
-lines terminated by '\r\n' 
-ignore 1 lines;
-show warnings; 
+load data local infile 'UMLS_TTY.tsv'  into table umls_tty  ignore 1 lines;
+show warnings;
 
 call create_index('umls_tty','TTY');
 
@@ -43,17 +38,12 @@ call log('umls_rel', 'relationships');
 drop table if exists umls_rel;
 create table umls_rel
 (
- REL varchar(8)	NOT NULL,
- REL_STR   varchar(50) 	NULL
+ REL        varchar(8)	NOT NULL,
+ REL_STR    varchar(70) NOT NULL
 );    
 
-load data local infile 'UMLS_REL.tsv' into table umls_rel
-fields terminated by '\t'
-optionally enclosed by '"' 
-ESCAPED BY '' 
-lines terminated by '\r\n' 
-ignore 1 lines;
-show warnings; 
+load data local infile 'UMLS_REL.tsv' into table umls_rel ignore 1 lines;
+show warnings;
 
 call create_index('umls_rel','REL');
 
@@ -63,17 +53,12 @@ call log('umls_rela', 'relationship attributes');
 drop table if exists umls_rela;
 create table umls_rela
 (
- RELA varchar(100)	NOT NULL,
+ RELA       varchar(100)	NOT NULL,
  RELA_STR   varchar(100) 	NULL
 );    
 
-load data local infile 'UMLS_RELA.tsv' into table umls_rela 
-fields terminated by '\t'
-optionally enclosed by '"' 
-ESCAPED BY '' 
-lines terminated by '\r\n' 
-ignore 1 lines;
-show warnings; 
+load data local infile 'UMLS_RELA.tsv' into table umls_rela ignore 1 lines;
+show warnings;
 
 call create_index('umls_rela','RELA');
 
