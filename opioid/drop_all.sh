e9ac479aa018b912d21a6f2b2ed288dd2d44758a
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-04-25 04:07:24 -0700

    drop_all.sh helper target added
    verify.sql removed (accidental checkin)

---
+++ opioid/drop_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+source db.config
+source env_table_schema.sh
+
+export CURATED="vsac_math"
+./drop_database.sh
+
+export CURATED="bioportal"
+./drop_database.sh
+
+export CURATED="bioportal_to_umls"
+./drop_database.sh
+
+export CURATED="wasz_april7"
+./drop_database.sh
+
+export CURATED="custom_rxcui_str"
+./drop_database.sh
+
+export CURATED="all_rxcui_str"
+./drop_database.sh
+
--- opioid/verify.sql
+++
@@ -1,4 +0,0 @@
-select curated, count(distinct RXCUI) cnt
-from RXNCONSO_curated_jaccard
-group by curated
-order by cnt desc;
