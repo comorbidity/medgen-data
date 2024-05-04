1d4c8bb1664d1a0f17d1c03c65ab374b14592286
comorbidity <andrew.mcmurry@tch.harvard.edu>
2024-05-04 14:30:11 -0400

    added cat (concat) files, the aggregation of the fully expanded (expand_rxcui#_str#)

---
+++ opioid/infile/link_cat.sh
@@ -0,0 +1,24 @@
+CURATED="vsac_math"
+rm $CURATED.tsv
+ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+
+CURATED="medrt"
+rm $CURATED.tsv
+ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+
+CURATED="bioportal"
+rm $CURATED.tsv
+ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+
+CURATED="custom_rxcui_str"
+rm $CURATED.tsv
+ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+
+CURATED="bioportal_to_umls"
+rm $CURATED.tsv
+ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+
+CURATED="wasz_april7"
+rm $CURATED.tsv
+ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+
--- opioid/infile/link_orig.sh
+++ opioid/infile/link_orig.sh
@@ -1,19 +1,23 @@
 CURATED="vsac_math"
 rm $CURATED.tsv
-ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+ln -s $CURATED/$CURATED.curated.tsv $CURATED.tsv
 
 CURATED="medrt"
 rm $CURATED.tsv
-ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+ln -s $CURATED/$CURATED.curated.tsv $CURATED.tsv
 
 CURATED="bioportal"
 rm $CURATED.tsv
-ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+ln -s $CURATED/$CURATED.curated.tsv $CURATED.tsv
+
+CURATED="custom_rxcui_str"
+rm $CURATED.tsv
+ln -s $CURATED/$CURATED.curated.tsv $CURATED.tsv
 
 CURATED="bioportal_to_umls"
 rm $CURATED.tsv
-ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+ln -s $CURATED/$CURATED.curated.tsv $CURATED.tsv
 
 CURATED="wasz_april7"
 rm $CURATED.tsv
-ln -s $CURATED/$CURATED.cat.tsv $CURATED.tsv
+ln -s $CURATED/$CURATED.curated.tsv $CURATED.tsv
